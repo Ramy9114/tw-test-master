0137a34fdc144daeb20809f5f1481c1e8bdfd236
Rami Moubayed <rmoubayed12@gmail.com>
2020-07-29 09:30:20 +0200

    adding files

---
+++ routes/web/tws.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Router = express.Router();
+
+const Tw = require('../../models/Tw');
+
+Router.get('/', (req, res) => {
+    var error = false;
+
+    Tw.find()
+        .sort({createdAt:-1})
+        .lean()
+        .exec()
+        .then(tws => {
+            res.render('tws', {
+                tws: tws,
+                error: error
+            });
+        })
+        .catch(err => {
+            error = err;
+            console.error(error);
+        });
+});
+
+module.exports = Router;
